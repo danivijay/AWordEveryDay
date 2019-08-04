454ca291ee52c50a772bb19f79f4054d98108eb4
danivijay <danimvijay@gmail.com>
2019-08-05 02:09:43 +0530

    feat: add docs folder for testing

---
+++ docs/precache-manifest.f66d08e3da60e6d83528af45fe6efcc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c911e177a18cdb533a451b56372bb904",
+    "url": "/AWordEveryDay/index.html"
+  },
+  {
+    "revision": "de12cfe70d7c7a5f19e8",
+    "url": "/AWordEveryDay/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4dc679990607a807ee1c",
+    "url": "/AWordEveryDay/static/js/2.fbfed12d.chunk.js"
+  },
+  {
+    "revision": "de12cfe70d7c7a5f19e8",
+    "url": "/AWordEveryDay/static/js/main.239b851b.chunk.js"
+  },
+  {
+    "revision": "df9e31f0820cf0156f72",
+    "url": "/AWordEveryDay/static/js/runtime~main.b4f59e5b.js"
+  }
+]);
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,9 @@ const Hero = () => {
                   <Columns isCentered style={{ marginTop: '20px' }} isFullWidth>
                         <Column isSize={{ mobile: 12, desktop: '1/2' }} hasTextAlign="centered">
                               <Title isSize={1}>
-                                    #A<span style={{ color: '#0967fd' }}>Word</span>EveryDay
+                                    #A<span style={{ color: '#0967fd' }}>Word</span>
+                                    <wbr />
+                                    EveryDay
                               </Title>
                               <Subtitle style={{ marginTop: '40px' }} isSize={4}>
                                     I'm learning a word everyday(well.. almost), with the help of pop culture. Each word will have a real life
