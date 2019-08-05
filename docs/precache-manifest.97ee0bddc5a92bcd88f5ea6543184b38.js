ab7813b91fb2df4df23217860c0dca23a937ed25
danivijay <danimvijay@gmail.com>
2019-08-05 12:53:08 +0530

    fix: homepage to custom domain in package.json

---
+++ docs/precache-manifest.97ee0bddc5a92bcd88f5ea6543184b38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2186f694a555cc318fcd0311dbc12b41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2895029b057a0f98a285",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4dc679990607a807ee1c",
+    "url": "/static/js/2.fbfed12d.chunk.js"
+  },
+  {
+    "revision": "2895029b057a0f98a285",
+    "url": "/static/js/main.c713d688.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AWordEveryDay/precache-manifest.144a2786b11a639620788112808d2a2a.js"
+  "/precache-manifest.97ee0bddc5a92bcd88f5ea6543184b38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AWordEveryDay/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
