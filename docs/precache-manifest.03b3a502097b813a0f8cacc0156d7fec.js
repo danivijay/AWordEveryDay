cd0e758123765804c60f23331cd7bf2f94f7daf7
danivijay <danimvijay@gmail.com>
2019-08-07 11:28:58 +0530

    feat: change icons, add word Precursor

--- docs/precache-manifest.03b3a502097b813a0f8cacc0156d7fec.js
+++ docs/precache-manifest.03b3a502097b813a0f8cacc0156d7fec.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7524fa6d572837ff58c14d62454a3039",
+    "revision": "dbcfe5cd547b78b9bd85f1754bb50e38",
     "url": "/index.html"
   },
   {
-    "revision": "460a17503310db066553",
+    "revision": "ecbbd851e136e183792f",
     "url": "/static/css/main.842ecbec.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.fa4255ae.chunk.js"
   },
   {
-    "revision": "460a17503310db066553",
-    "url": "/static/js/main.754fdabe.chunk.js"
+    "revision": "ecbbd851e136e183792f",
+    "url": "/static/js/main.eae065d0.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e9112a2d4725a9a6222fd1901d9e816.js"
+  "/precache-manifest.03b3a502097b813a0f8cacc0156d7fec.js"
 );
 
 self.addEventListener('message', (event) => {
