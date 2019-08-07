cc31a53b08fcd2a2b87b16c15584d1c2ae623175
danivijay <danimvijay@gmail.com>
2019-08-07 11:35:59 +0530

    feat: add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import wordListJson from './wordList.json';
 import WordList from './components/WordList';
 import Footer from './components/Footer';
 import './app.css';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-93459619-4');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+console.log(window.location.pathname + window.location.search);
 
 function App() {
       return (
